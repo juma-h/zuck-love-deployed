fd0b853582536cfe15a87c6c96923bb932f71dc6
Juma-h <mitchfibi@gmail.com>
2023-08-23 14:21:24 +0300

    css modifications

--- src/components/Navtabs/Navtabs.js
+++ src/components/Navtabs/Navtabs.js
@@ -8,18 +8,21 @@ const NavTabs = () => {
     setActiveTab(index);
   };
 
-  const tabs = [
-    "Variation 1 ",
-    "Variation 2",
-    "Variation 3",
-    "Variation 4"
-  ];
+  const tabs = ["Variation 1 ", "Variation 2", "Variation 3", "Variation 4"];
 
   const tabContents = [
-    "This is the content for Variation 1.",
+    `🚀Transform Your Health Journey Overseas with Ease! 🌍\n
+  Are you tired of the daunting task of finding the right overseas doctor? Say goodbye to the struggle with Turkey's leading gastric sleeve surgery agency! We're more than just an agency; we're your personalized healthcare architects, building you an all-inclusive, custom-tailored medical journey. 💖\n
+  🎯 Here's why we are your top choice:\n
+  ✈️ Enjoy hassle-free round-trip flights\n
+  🏨 Rest and relax in luxury 4-5 star hotels\n
+  👩‍⚕️ Benefit from thorough pre and post-op consultations, designed to empower you with confidence\n
+  🩹 Experience a comprehensive aftercare plan, giving you peace of mind during recovery\n
+  But don't just trust us! 🗣️ Let the testimonies of our numerous satisfied patients speak for our commitment and exceptional service. 🌟\n
+  Ready to redefine your healthcare experience? Simply tap 'Learn More' to commence your seamless, stress-free gastric sleeve surgery journey in Turkey. Your ideal doctor is just one click away! 👆💙`,
     "This is the content for variation 2.",
     "This is the content for variation 3.",
-    "This is the content for variation 4."
+    "This is the content for variation 4.",
   ];
 
   return (
@@ -27,21 +30,23 @@ const NavTabs = () => {
       <ul className="nav nav-pills nav-justified tab-buttons">
         {tabs.map((tab, index) => (
           <li className="nav-item variation-tab" key={index}>
-            <button
-              className={`nav-link ${activeTab === index ? "active" : "inactive"}`}
+            <a
+              className={`nav-link ${
+                activeTab === index ? "active" : "inactive"
+              }`}
               onClick={() => handleTabClick(index)}
+              href
             >
-              {tab} <i class="fa-regular fa-circle-xmark fa-2xs"></i>
-            </button>
+              <span>{tab}</span>
+              <i class="fa-regular fa-circle-xmark fa-2xs close-icon"></i>
+            </a>
           </li>
         ))}
       </ul>
       <div className="tab-content-container">
-        <div className="tab-content">
-          <p>
-            {tabContents[activeTab]}
-          </p>
-        </div>
+        <textarea className="tab-content" readOnly>
+          {tabContents[activeTab]}
+        </textarea>
       </div>
     </div>
   );
--- src/components/VariantDiv/VariantDiv.js
+++ src/components/VariantDiv/VariantDiv.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./variant.css";
 import NavTabs from "../Navtabs/Navtabs";
+import Box from "@mui/material/Box";
+import InputAdornment from "@mui/material/InputAdornment";
+import TextField from "@mui/material/TextField";
 
 const VariantDiv = () => {
   return (
@@ -13,28 +16,28 @@ const VariantDiv = () => {
         sit arcu curabitur.
       </p>
       <div className="variant-select-div row">
-        <div className="col-md-7">
-          Select the following details below:
-          <div className="row">
+        <div className="col-md-7 mt-5">
+          <p className="select-label">Select the following details below:</p>
+          <div className="row ">
             <div className="col-md-4">
-              <select class="form-select" aria-label="Select 1">
-                <option selected>Open this select menu</option>
+              <select class="form-select  select-css" aria-label="Select 1">
+                <option selected>Campaign</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
               </select>
             </div>
             <div className="col-md-4">
-              <select class="form-select" aria-label="Select 2">
-                <option selected>Open this select menu</option>
+              <select class="form-select select-css" aria-label="Select 2">
+                <option selected>Adset</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
               </select>
             </div>
             <div className="col-md-4">
-              <select class="form-select" aria-label="Select 3">
-                <option selected>Open this select menu</option>
+              <select class="form-select select-css" aria-label="Select 3">
+                <option selected>Ad</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
@@ -46,34 +49,69 @@ const VariantDiv = () => {
         <div className="col-md-1">
           <hr className="vertical-hr" />
         </div>
-        <div className="col-md-4">
-          Metric to optimize for:
-          <select class="form-select" aria-label="Select 3">
-            <option selected>Open this select menu</option>
+        <div className="col-md-4 mt-5">
+          <p className="select-label">Metric to optimize for:</p>
+          <select class="form-select select-css" aria-label="Select 3">
+            <option selected>Outbound CTR</option>
             <option value="1">One</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
           </select>
-          <p>
-            <em>Tip:</em>Events that get fired the most will reach statistically
+          <small className="helper-text">
+            <b>Tip:</b>Events that get fired the most will reach statistically
             significance faster.
-          </p>
+          </small>
         </div>
-        <div>
-          <p>When should we consider this results signifcant?</p>
-          <p>
+        {/* <div className="input-label">
+          <h5>When should we consider these results significant?</h5>
+          <p style={{ display: "flex", alignItems: "center" }}>
             When you reach
-            <input /> %,certainity, consider the results statistically
-            significant
+            <TextField
+              id="standard-end-adornment"
+              sx={{ m: 1, width: "12ch" }}
+              size="small"
+              InputProps={{
+                endAdornment: <InputAdornment position="end">%</InputAdornment>,
+              }}
+              helperText="Tip: 95% is considered standard"
+              variant="outlined"
+              style={{ "--helper-text-width": "300px" }}
+            />
+            
+            certainty, consider the results statistically significant.
           </p>
+      
+        </div> */}
+        <div className="input-label">
+          <h5>When should we consider these results significant?</h5>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <span>When you reach</span>
+            <TextField
+              id="standard-end-adornment"
+              sx={{ m: 1, width: "12ch" }}
+              size="small"
+              InputProps={{
+                endAdornment: <InputAdornment position="end">%</InputAdornment>,
+              }}
+              variant="outlined"
+            />
+            <span>
+              , certainty, consider the results statistically significant.
+            </span>
+          </div>
+          <div style={{ marginLeft: "110px" }}>
+            <small className="helper-text"> 
+            <i class="fa-solid fa-circle-info"></i>
+             <b>Tip:</b> 95% is considered standard
+            </small>
+          </div>
         </div>
-        <hr></hr>
+
+        <hr className="horizontal-hr"></hr>
 
         <NavTabs />
-        <div>
-          <p for="exampleFormControlInput1" class="form-label">
-            New Ad Name
-          </p>
+        <div className="mt-5">
+          <p style={{textAlign:"left", fontSize:"20px"}}>New Ad Name</p>
           <div class="mb-3">
             <input
               type="text"
@@ -82,7 +120,7 @@ const VariantDiv = () => {
               placeholder="Type something here"
             />
           </div>
-          <button className="btn btn-primary btn-bg">Launch Test</button>
+          <button className="btn btn-primary btn-bg launch-btn">Launch Test</button>
         </div>
       </div>
     </div>
