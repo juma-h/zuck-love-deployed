5617964043a5e3e93d76ead8a6c531cb0fbd90af
Juma-h <mitchfibi@gmail.com>
2023-08-24 14:33:22 +0300

    react routes , and changed font to poppins

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ZuckLove } from './pages';
-import './App.css';
+import { ZuckLove, Copy, Headlines, ImageTest , Product} from "./pages";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-       <div className="App">
+      <div className="App">
         <Routes>
-          <Route path="/" element={<ZuckLove/>}/>
+          <Route path="/" element={<ZuckLove />}>
+            <Route index element={<Copy />} />
+            <Route path="/headlines" element={<Headlines />} />
+            <Route path="/product" element={<Product/>}/>
+            <Route path="/images" element={<ImageTest />} />
+          </Route>
         </Routes>
-     
-     </div>
+      </div>
     </Router>
-   
   );
 }
 
