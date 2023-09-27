53b5c17b910e86334e539a601d6a46ed031aff8e
Juma-h <mitchfibi@gmail.com>
2023-09-27 17:03:03 +0300

    toast width css responsiveness

--- src/index.js
+++ src/index.js
@@ -6,27 +6,25 @@ import App from "./App";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-  <Router>
-  <App />
-    <ToastContainer
-    className="custom-toast-container"
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="colored"
-    />
-  </Router>
-
+    <Router>
+      <App />
+      <ToastContainer
+        className="custom-toast-container"
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </Router>
   </>
 );
