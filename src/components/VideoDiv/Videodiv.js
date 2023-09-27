e7a16160a20122b1d3fe29a69138ca858b8c47e8
Juma-h <mitchfibi@gmail.com>
2023-09-27 16:58:19 +0300

    video component responsivess

--- src/components/VideoDiv/Videodiv.js
+++ src/components/VideoDiv/Videodiv.js
@@ -9,13 +9,12 @@ const VideoDiv = () => {
     <div className="video-container">
       <h3 className="video-h">Need Help? </h3>
       <p className="video-p">
-        You can read through our documentation here or watch our quick videos to
-        get started
+        You can read through our documentation here or watch our quick videos to get started
       </p>
       <p className="video-p2">
-        Get started with copy testing in 2 minutes also <b>Support is available through whatsapp</b>
+        Get started with copy testing in 2 minutes also <b>Support is available through WhatsApp</b>
       </p>
-      {/* get started , whatsapp  */}
+
       <div className="video-buttons m-3">
         <button className="btn btn-primary btn-lg">Get Started</button>
 
@@ -51,18 +50,18 @@ const VideoDiv = () => {
           }}
         />
       </div>
-      {/* video div */}
 
       <div className="mt-5 video-iframe">
         <iframe
           src="https://www.youtube.com/embed/uXWycyeTeCs"
-          width={1000}
-          height={500}
+          width="100%" /* Use percentage width to make it responsive */
+          height="600px" /* Set height to auto for responsive aspect ratio */
           title="A youtube video on React hooks"
         ></iframe>
       </div>
     </div>
   );
 };
+
 
 export default VideoDiv;
